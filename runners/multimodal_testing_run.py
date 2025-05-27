e46aba517f9e3bffb6eb53daa6cef5d5cce3afb0
AmayaGS <a.r.syed@qmul.ac.uk>
2025-05-27 16:03:31 +0100

    heatmap utils

--- runners/multimodal_testing_run.py
+++ runners/multimodal_testing_run.py
@@ -85,29 +85,33 @@ def test_multimodal_model(config, is_continuation=False, is_full_train=False, ex
             from runners.gene_pathway_analysis_run import run_gene_pathway_analysis
             from utils.simple_visualizations import create_plots
 
-            # vis_results = r"C:\Users\Amaya\Documents\PhD\ProtoPathway\output\experiments\MM-MM-ProtoPathway_FT_fl-2-128_ds-R4RA_lr-1.0e-5_bs-1_dr-0.2_l1-0_l2-0_nl-0_hd-128_20250525_180450\visualise\vis_dict.pkl"
-            # with open(vis_results, 'rb') as f:
-            #     test_results = pickle.load(f)
-            #
+            vis_results = r"C:\Users\Amaya\Documents\PhD\ProtoPathway\output\experiments\MM-MM-ProtoPathway_FT_fl-2-128_ds-R4RA_lr-1.0e-5_bs-1_dr-0.2_l1-0_l2-0_nl-0_hd-128_20250525_180450\visualise\vis_dict.pkl"
+            with open(vis_results, 'rb') as f:
+                test_results = pickle.load(f)
+
             # attention_dict = test_results['metrics']['attention_dict']
             # run_gene_pathway_analysis(config, attention_dict, wsi_features, test_results_dir, experiment_logger)
 
-            plots_dir = os.path.join(test_results_dir, 'plots')
-            create_plots(test_results_dir, plots_dir, config)
+            # plots_dir = os.path.join(test_results_dir, 'plots')
+            # create_plots(test_results_dir, plots_dir, config)
+
+            # HEATMAPS
+
 
             # ####################################################################################
-            # patient_ids = list(test_results['metrics']['attention_dict'].keys())[-2]
-            # gene_idx = attention_dict['gene_idx']
-            # pathway_idx = attention_dict['pathway_idx']
-            #
-            # gene_idx_inv = {v.item(): k for k, v in gene_idx.items()}
-            # pathway_idx_inv = {v.item(): k for k, v in pathway_idx.items()}
-            #
-            # for patient_id in patient_ids:
-            #     patient_info = attention_dict[patient_id]
-            #     wsi_patch_names = wsi_features[patient_id][2]['filenames']
-            #     patch_asssignment = patient_info['hard_assignments'].squeeze(0)
-            #     patch_asssignment = [p.item() for p in patch_asssignment]
+            patient_ids = list(test_results['metrics']['attention_dict'].keys())[:-2]
+            attention_dict = test_results['metrics']['attention_dict']
+            gene_idx = attention_dict['gene_idx']
+            pathway_idx = attention_dict['pathway_idx']
+
+            gene_idx_inv = {v.item(): k for k, v in gene_idx.items()}
+            pathway_idx_inv = {v.item(): k for k, v in pathway_idx.items()}
+
+            for patient_id in patient_ids:
+                patient_info = attention_dict[patient_id]
+                wsi_patch_names = wsi_features[patient_id][2]['filenames']
+                patch_asssignment = patient_info['hard_assignments'].squeeze(0)
+                patch_asssignment = [p.item() for p in patch_asssignment]
             #     sorted_gene_importance = patient_info['gene_pathway_attn'].sum(dim=1).sort()
 
 
